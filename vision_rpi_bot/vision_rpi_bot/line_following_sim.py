9cbcef2061e05190de71b7271bf397f0d2f1372c
thejediboySHASHANK <21052358@kiit.ac.in>
2023-10-25 16:40:45 +0530

    version2

--- vision_rpi_bot/vision_rpi_bot/line_following_sim.py
+++ vision_rpi_bot/vision_rpi_bot/line_following_sim.py
@@ -11,8 +11,12 @@ class camera_sub(Node):
         super().__init__('qr_maze_solving_node')
         self.camera_sub = self.create_subscription(Image, '/vision_rpi_bot_camera/image_raw', self.camera_cb, 10)
         self.bridge = CvBridge()
+        
 
     def camera_cb(self, data):
+        x_last = 320
+        y_last = 180
+
         camera = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         Blackline = cv2.inRange(camera, (0,0,0), (60,60,60))
         Greensign = cv2.inRange(camera, (0, 65, 0), (100, 200, 100))
@@ -49,20 +53,52 @@ class camera_sub(Node):
             centerx_grn = x_grn + (w_grn/2)	   
             cv2.line(camera, (int(centerx_grn), 200), (int(centerx_grn), 250), (0, 0, 255), 3)
 
+        contours_blk_len = len(contours_blk)
         if len(contours_blk) > 0:
             x_blk, y_blk, w_blk, h_blk = cv2.boundingRect(contours_blk[0])
-            centerx_blk = x_blk + (w_blk/2)	  
-            #2nd addition 	   
-            blackbox = cv2.minAreaRect(contours_blk[0])
+            centerx_blk = x_blk + (w_blk/2)
+
+            #3rd addition
+            if contours_blk_len == 1:
+                blackbox = cv2.minAreaRect(contours_blk[0])
+            else:
+                canditates = []
+                off_bottom = 0
+                for con_num in range(contours_blk_len):
+                    blackbox = cv2.minAreaRect(contours_blk[con_num])
+                    (x_min, y_min), (w_min, h_min), ang = blackbox
+                    box = cv2.boxPoints(blackbox)
+                    (x_box,y_box) = box[0]
+                    if y_box > 358 :
+                        off_bottom += 1
+                    canditates.append((y_box,con_num,x_min,y_min))	
+                canditates = sorted(canditates)
+
+                if off_bottom > 1:
+                    canditates_off_bottom=[]
+                    for con_num in range ((contours_blk_len - off_bottom), contours_blk_len):
+                        (y_highest,con_highest,x_min, y_min) = canditates[con_num]
+                        total_distance = (abs(x_min - x_last)**2 + abs(y_min - y_last)**2)**0.5
+                        canditates_off_bottom.append((total_distance,con_highest))
+                    canditates_off_bottom = sorted(canditates_off_bottom)
+                    (total_distance,con_highest) = canditates_off_bottom[0] 
+                    blackbox = cv2.minAreaRect(contours_blk[con_highest])	
+                else:
+                    (y_highest,con_highest,x_min, y_min) = canditates[contours_blk_len-1]
+                    blackbox = cv2.minAreaRect(contours_blk[con_highest])
+            
             (x_min, y_min), (w_min, h_min), ang = blackbox
+            x_last = x_min
+            y_last = y_min
             if ang < -45 :
                 ang = 90 + ang
-            if w_min < h_min and ang > 0:	  
+            if w_min < h_min and ang > 0:	
                 ang = (90-ang)*-1
             if w_min > h_min and ang < 0:
                 ang = 90 + ang
             setpoint = 320
             error = int(x_min - setpoint) 
+            ang = int(ang)
             ang = int(ang)	 
             if (ang == 90) :
                 ang = ang - 1
@@ -75,7 +111,8 @@ class camera_sub(Node):
             cv2.drawContours(camera,[box],0,(0,0,255),3)	 
             cv2.putText(camera,str(ang),(10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.putText(camera,str(error),(10, 320), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.line(camera, (int(x_min),200 ), (int(x_min),250 ), (255,0,0),3)
+            cv2.line(camera, (int(x_min),200 ), (int(x_min),250 ), (255,0,0),3)	
+            #2nd addition 	   
 
         if Greendected: 
             if centerx_grn > centerx_blk:
@@ -102,6 +139,8 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
 
 
 
@@ -217,7 +256,7 @@ if __name__ == '__main__':
 
 # def main(args=None):
 #     rclpy.init(args=args)
-#     sensor_sub = CameraSub()
+#     sensor_sub = Came(total_distance,con_highest) = canditates_off_bottom[0] raSub()
 #     rclpy.spin(sensor_sub)
 #     sensor_sub.destroy_node()
 #     rclpy.shutdown()
